97c6222941975096d92c7415496d74117c87be42
julia <juliia.sukhareva@gmail.com>
2026-06-19 02:31:05 +0400

    Add Tapsiriq6

---
+++ src/Tapsiriq6.java
@@ -0,0 +1,18 @@
+public class Tapsiriq6 {
+    public static void main(String[] args) {
+
+        int a = 17;
+        int b = 5;
+
+        System.out.println("Dord Emeliyyat");
+        System.out.println("a = " + a + ", b = " + b);
+
+        System.out.println("Toplama: " + a + " + " + b + " = " + (a + b));
+        System.out.println("Cixma: " + a + " - " + b + " = " + (a - b));
+        System.out.println("Vurma: " + a + " * " + b + " = " + (a * b));
+        System.out.println("Bolme: " + a + " / " + b + " = " + (a / b));
+        System.out.println("Qaliq: " + a + " % " + b + " = " + (a % b));
+
+    }
+}
+
