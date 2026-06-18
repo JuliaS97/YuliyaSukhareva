c0f50d2c1e632af06f915530d1f0b2dd2190bc54
julia <juliia.sukhareva@gmail.com>
2026-06-19 02:24:51 +0400

    Add Tapsiriq3

---
+++ src/Tapsiriq3.java
@@ -0,0 +1,18 @@
+public class Tapsiriq3 {
+    public static void main(String[] args) {
+
+        final int MIN_KREDIT = 300;
+        final int MAX_KREDIT = 50000;
+        final double FAIZ = 10.9;
+        final String BANK_ADI = "ABB";
+        final int MAX_MUDDET = 60;
+
+        System.out.println("Bank Sabit Parametrləri");
+        System.out.println("Min kredit meblegi: " + MIN_KREDIT + " AZN");
+        System.out.println("Max kredit meblegi: " + MAX_KREDIT + " AZN");
+        System.out.println("Illik faiz: " + FAIZ + "%");
+        System.out.println("Bankin adi: " + BANK_ADI);
+        System.out.println("Max kredit muddeti: " + MAX_MUDDET + " ay");
+
+    }
+}
