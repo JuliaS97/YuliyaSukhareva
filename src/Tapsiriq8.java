da0dcdf3ce5c2bd602a7bf0d1ca83ff67f80d529
julia <juliia.sukhareva@gmail.com>
2026-06-19 02:38:13 +0400

    Add Tapsiriq8

---
+++ src/Tapsiriq8.java
@@ -0,0 +1,29 @@
+public class Tapsiriq8 {
+    public static void main(String[] args) {
+
+        int c1 = 100;
+        double f1 = c1 * 9 / 5 + 32;
+        System.out.println("Yanlish");
+        System.out.println("100°C = " + f1 + "°F");
+
+        int c2 = 37;
+        double f2 = c2 * 9 / 5 + 32;
+        System.out.println("37°C  = " + f2 + "°F");
+
+        System.out.println();
+
+        System.out.println("Duzgun");
+
+        double c3 = 0;
+        double f3 = c3 * 9.0 / 5 + 32;
+        System.out.println("0°C   = " + f3 + "°F");
+
+        double c4 = 100;
+        double f4 = c4 * 9.0 / 5 + 32;
+        System.out.println("100°C = " + f4 + "°F");
+
+        double c5 = 37;
+        double f5 = c5 * 9.0 / 5 + 32;
+        System.out.println("37°C  = " + f5 + "°F");
+    }
+}
