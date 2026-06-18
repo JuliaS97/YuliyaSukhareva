dc1bf0abc7c6096eaa4baf31c3fc3b6dbf9d117b
julia <juliia.sukhareva@gmail.com>
2026-06-19 02:48:41 +0400

    Add Tapsiriq11

---
+++ src/Tapsiriq11.java
@@ -0,0 +1,20 @@
+public class Tapsiriq11 {
+    public static void main(String[] args) {
+
+        int x = 18;
+        int y = 18;
+        int z = 25;
+
+        System.out.println("Muqayise Neticeleri");
+        System.out.println("x = " + x + ", y = " + y + ", z = " + z);
+
+
+        System.out.println("x == y -> " + (x == y));
+        System.out.println("x == z -> " + (x == z));
+        System.out.println("x != z -> " + (x != z));
+        System.out.println("z > x -> " + (z > x));
+        System.out.println("x >= y -> " + (x >= y));
+        System.out.println("y <= z -> " + (y <= z));
+
+    }
+}
