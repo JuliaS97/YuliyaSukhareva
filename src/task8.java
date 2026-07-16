86aab3a52ab3a140b1f3ce2409f4ce1065689176
julia <juliia.sukhareva@gmail.com>
2026-07-16 22:56:24 +0400

    Add task8

---
+++ src/task8.java
@@ -0,0 +1,16 @@
+public class task8 {
+    public static void main(String[] args) {
+
+        int[] ballar = {67, 91, 54, 88, 73};
+
+        int enBoyuk = ballar[0];
+
+        for (int bal : ballar) {
+            if (bal > enBoyuk) {
+                enBoyuk = bal;
+            }
+        }
+
+        System.out.println("Ən böyük bal: " + enBoyuk);
+    }
+}
