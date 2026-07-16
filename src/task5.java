b0f28d4d407699b263c04075ef08710eab0ed598
julia <juliia.sukhareva@gmail.com>
2026-07-16 22:53:34 +0400

    Add task5

---
+++ src/task5.java
@@ -0,0 +1,11 @@
+public class task5 {
+    public static void main(String[] args) {
+        int[] ededler = {4, 8, 15, 16, 23, 42};
+
+        for (int eded : ededler) {
+            System.out.println(eded);
+        }
+
+        System.out.println("Element sayı: " + ededler.length);
+    }
+}
