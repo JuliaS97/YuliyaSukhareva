40864c9804ea459c5d720eed17329224ee0d34d2
julia <juliia.sukhareva@gmail.com>
2026-07-20 22:24:01 +0400

    Remove all files

--- src/task6.java
+++
@@ -1,9 +0,0 @@
-public class task6 {
-    public static void main(String[] args) {
-        String[] adlar = {"Aysel", "Murad", "Leyla", "Tural"};
-
-        for (String ad : adlar) {
-            System.out.println("Salam, " + ad + "!");
-        }
-    }
-}
